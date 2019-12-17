f7ebe0a460352aae1a54c8239b665278e7f3f47a
kimsuazo <quim.suazo@gmail.com>
2019-12-17 13:33:29 +0100

    versio eloi-down

--- Kannada_MNIST_TF.py
+++ Kannada_MNIST_TF.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import idx2numpy
 import pandas as pd
-import tensorflow as tf #version 2.0
+import tensorflow.compat.v1 as tf #version 2.0
 from tensorflow import data
 '''
 # %% [code]
@@ -30,31 +30,27 @@ logdir='/home/jamao/Desktop/programes/Kaggle_shared_projects/tensorboard_logs'
 
 # %% [code]
 #Local paths to data files:
-test_X = '/home/jamao/Desktop/programes/data/Fashion MNIST/t10k-images-idx3-ubyte'
-test_y = '/home/jamao/Desktop/programes/data/Fashion MNIST/t10k-labels-idx1-ubyte'
-train_X = '/home/jamao/Desktop/programes/data/Fashion MNIST/train-images-idx3-ubyte'
-train_y = '/home/jamao/Desktop/programes/data/Fashion MNIST/train-labels-idx1-ubyte'
+test_X = '/home/kimxu/Documents/kaggle/Kaggle-shared_projects/FashionData/t10k-images-idx3-ubyte'
+test_y = '/home/kimxu/Documents/kaggle/Kaggle-shared_projects/FashionData/t10k-labels-idx1-ubyte'
+train_X = '/home/kimxu/Documents/kaggle/Kaggle-shared_projects/FashionData/train-images-idx3-ubyte'
+train_y = '/home/kimxu/Documents/kaggle/Kaggle-shared_projects/FashionData/train-labels-idx1-ubyte'
 
 #load data locally
-train_X = idx2numpy.convert_from_file(train_X).reshape((60000,-1))
+train_X = idx2numpy.convert_from_file(train_X).reshape((60000,-1))/255.
 train_y = idx2numpy.convert_from_file(train_y)
-test_X = idx2numpy.convert_from_file(test_X).reshape((10000,-1))
+test_X = idx2numpy.convert_from_file(test_X).reshape((10000,-1))/255.
 test_y = idx2numpy.convert_from_file(test_y)
 
 
-train_dataset = tf.data.Dataset.from_tensor_slices((train_X, train_y))
-test_dataset = tf.data.Dataset.from_tensor_slices((test_X, test_y))
-
-
 # %% [code]
-graph = tf.compat.v1.Graph()
+graph = tf.Graph()
 
 with graph.as_default():
         
-    with tf.compat.v1.variable_scope("Input"):
+    with tf.variable_scope("Input"):
         
-        train_dataset = tf.data.Dataset.from_tensor_slices((train_dataset, train_y))
-        test_dataset = tf.data.Dataset.from_tensor_slices((test_dataset, test_y))
+        train_dataset = tf.data.Dataset.from_tensor_slices((train_X, train_y))
+        test_dataset = tf.data.Dataset.from_tensor_slices((test_X, test_y))
         
         train = train_dataset.shuffle(10)
         train = train.batch(32)
@@ -71,25 +67,25 @@ with graph.as_default():
         x, y = train_batch #to compute now, no placeholder or anything
         x = tf.reshape(x, [-1,28,28,1])
         
-    with tf.compat.v1.variable_scope("FeedForward"):
+    with tf.variable_scope("FeedForward"):
         
-        with tf.compat.v1.variable_scope("Conv_Bloc_1"):
-            x = tf.keras.layers.Conv2d(filters = 6, kernel_size = (5,5), padding = (2,2), activation = 'relu', name="conv1")(x)
+        with tf.variable_scope("Conv_Bloc_1"):
+            x = tf.keras.layers.Conv2D(filters = 6, kernel_size = (5,5), activation = 'relu', name="conv1")(x)
             x = tf.keras.layers.MaxPool2D(strides = (1,1), name="pool1")(x)
         
-        with tf.compat.v1.variable_scope("Conv_Bloc_2"):
-            x = tf.keras.layers.Conv2d(filters = 16, kernel_size = (5,5), activation = 'relu', name="conv1")(x)
+        with tf.variable_scope("Conv_Bloc_2"):
+            x = tf.keras.layers.Conv2D(filters = 16, kernel_size = (5,5), activation = 'relu', name="conv1")(x)
             x = tf.keras.layers.MaxPool2D(strides = (1,1), name="pool1")(x)
         
-        with tf.compat.v1.variable_scope("Dense_bloc"):
+        with tf.variable_scope("Dense_bloc"):
             x = tf.keras.layers.Flatten(data_format = 'channels_first')(x)
             x = tf.keras.layers.Dense(units=120, activation='relu')(x)
             x = tf.keras.layers.Dense(units=84, activation='relu')(x)
             y_ = tf.keras.layers.Dense(units=10, activation='softmax')(x)
     
-sess = tf.compat.v1.Session()
+sess = tf.Session()
 
 with sess:
-	sess.run(tf.compat.v1.global_variables_initializer())
+	sess.run(tf.global_variables_initializer())
 	result = sess.run(y_)
-	writer = tf.compat.v1.summary.FileWriter(logdir, graph=graph)
+	writer = tf.summary.FileWriter(logdir, graph=graph)
